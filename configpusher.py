8908fe986167e3603925568d213b8adaf4f69e7f
davidfine <dfine@bittorrent.com>
2016-01-03 13:20:47 -0800

    Initial comit, rough sketch of client funtionality

---
+++ configpusher.py
@@ -0,0 +1,12 @@
+
+
+class configpusher(object):
+    def bootstrap_remote_node(self, nodes):
+        '''
+        :param nodes: List of nodes on which to run
+        :return: 0 or 1 and list of nodes which raised exception
+        '''
+
+    def trigger_remote_config(self, nodes, post_data):
+        pass
+
